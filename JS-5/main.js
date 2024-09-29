35480e481041bd9529b8f719a5e9a1c367e8e4c0
ruddro42001~ <aliruddro@gmail.com>
2024-09-29 17:34:50 +0600

    JS Array and Object

---
+++ JS-5/main.js
@@ -0,0 +1,11 @@
+// JS Array
+const fruits = ['Apple', 'Banana', 'Mango'];
+fruits[1] = 'Orange';
+console.log(fruits); // Output: ['Apple', 'Orange', 'Mango']
+
+// JS Object
+const car = {type:"Fiat", model:"500", color:"white"};
+
+console.log(car.type) // Output: Fiat
+
+
