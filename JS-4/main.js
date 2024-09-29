f8bb634370fd95492f59f6549f8237c461b6fe49
ruddro42001~ <aliruddro@gmail.com>
2024-09-29 17:30:30 +0600

    JS Function

---
+++ JS-4/main.js
@@ -0,0 +1,16 @@
+// Function - Parameters + Arguments
+
+function twoNumberSum(a,b){
+    let sum = a + b;
+    console.log(sum)
+}
+twoNumberSum(10,20);
+
+
+// Function declaration with parameters
+function greet(name, age) {
+    console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+}
+
+// Function call with arguments
+greet('John', 30);
